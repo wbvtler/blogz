1449c601147c54d85a8005f4d094cd28dafbd91e
wbvtler <wesleybutler.photo@gmail.com>
2017-10-22 22:16:28 -0500

    db config, newpost and blog-home function

--- main.py
+++ main.py
@@ -6,13 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
-#TODO: 
-#Configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) # pylint: disable=locally-disabled, invalid-name
 
 #create secret key
 #app.secret_key = sdfasdfasdf
@@ -29,6 +26,7 @@ class Entry(db.Model):
         self.name = name
         self.text = text
 
+
 #### Routes ####
 @app.route('/')
 def to_mainpage():
@@ -36,11 +34,25 @@ def to_mainpage():
 
 @app.route('/blog')
 def blog():
-    return render_template('blog-home.html', title='Home')
+    entries = Entry.query.all()
+    return render_template('blog-home.html', title='Home', entries=entries)
 
-@app.route('/newpost')
+@app.route('/newpost', methods=['GET','POST'])
 def newpost():
+    if request.method == 'POST':
+        entry_name = request.form['name']
+        entry_text = request.form['text']
+        new_entry = Entry(name=entry_name, text=entry_text)
+        db.session.add(new_entry)
+        db.session.commit()
+        return redirect('/blog')
     return render_template('newpost.html')
+
+@app.route('/view-post')
+def view_post():
+    # TODO: 
+    # Use get request to select correct post
+    return render_template('view-post.html')
 
 
 if __name__ == '__main__':
