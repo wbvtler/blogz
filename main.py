aae3ef2e0dbe42d5a98d179a73f6fcda9d1de88f
wbvtler <wesleybutler.photo@gmail.com>
2017-10-30 12:24:20 -0500

    Add userId to session and indv-user.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#### configure environment ####
+            #### CONFFIGURE APP ####
 
 from flask import Flask, request, redirect
 from flask import render_template, session, flash
@@ -12,16 +12,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) # pylint: disable=locally-disabled, invalid-name
 
 #create secret key
-#app.secret_key = sdfasdfasdf
+app.secret_key = 'sdfasdfasdf'
 
 
-##### Classes ####
+            #### CLASSES ####
+
 class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16))
     password = db.Column(db.String(26))
-    blogs = db.relationship('Blog', backref='User', lazy=True)
+    blogs = db.relationship('Blog', backref='owner', lazy=True)
 
     def __init__(self, username, password):
         self.username = username
@@ -35,11 +36,28 @@ class Blog(db.Model):
     name = db.Column(db.String(120))
     text = db.Column(db.Text)
 
-    def __init__(self, name, text):
+    def __init__(self, name, text, owner_id):
         self.name = name
         self.text = text
+        self.owner_id = owner_id
 
 
+            #### ROUTES ####
+
+@app.before_request
+def requre_login():
+    blocked_routes = ['newpost']
+    if request.endpoint in blocked_routes and 'userId' not in session:
+        return redirect('login')
+
+# Working
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    print(session)
+    users = User.query.all()
+    return render_template('index.html', title='Index', users=users)
+
+# Working
 @app.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
 
@@ -65,29 +83,76 @@ def sign_up():
             new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
+            session['userId'] = new_user.id
             return redirect('/newpost')
         else:
             return render_template('sign-up.html', name_error=name_error, ver_pass_error=ver_pass_error, init_pass_error=init_pass_error, username=username)
     return render_template('sign-up.html')
 
+# Working
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    print(session)
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            # "remember" that the user has logged in
+            session['userId'] = user.id
+            flash('logged in')
+            return redirect('/blog')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+            return redirect('/')
+
+    return render_template('login.html')
+
+
+# Working
 @app.route('/newpost', methods=['GET','POST'])
 def newpost():
+    print(session)
     if request.method == 'POST':
         blog_name = request.form['name']
         blog_text = request.form['text']
-        new_blog = Blog(blog_name, blog_text)
+        user_id = session['userId']
+        new_blog = Blog(blog_name, blog_text, user_id)
         db.session.add(new_blog)
         db.session.commit()
-        blog_id = new_blog.id
         return redirect('/blog')
-        #return redirect('/view-post?id=' + entry_id)
     return render_template('newpost.html')
 
-
-@app.route('/blog')
+##### NOT WORKING #####
+# Fix for user with no posts
+# Add template for indv. users with username heading
+@app.route('/blog', methods=['GET', 'POST'])
 def blog_home():
+    print(session)
     blogs = Blog.query.all()
+    if not len(blogs) < 1:
+        ownerId = request.args.get('userId')
+        blogs = Blog.query.filter_by(owner_id=ownerId).all()
+        blog_owner = User.query.filter_by(id=ownerId).first()
+        owner_username = blog_owner.username
+        return render_template('indv-user.html', title=owner_username,
+    owner_username=owner_username, blogs=blogs)
     return render_template('blog-home.html', title='Home', blogs=blogs)
+
+#Working
+@app.route('/view-post')
+def viewpost():
+    print(session)
+    blog_id = request.args.get('blogId')
+    blog = Blog.query.filter_by(id=blog_id).first()
+    return render_template('view-post.html', blog=blog)
+
+# Working
+@app.route('/logout')
+def logout():
+    print(session)
+    del session['userId']
+    return redirect('blog')
 
 
 if __name__ == '__main__':
